5e04644e4aa957ad035ff1c2edd40b87e696d38c
Devansh <mailme.devanshkaushik@gmail.com>
2026-01-21 15:41:02 +0530

    script 12

---
+++ script12.js
@@ -0,0 +1,24 @@
+// let sentence = "I am a comma seperated sentence";
+// let words = sentence.split(" ");
+// console.log(words)
+
+// let sentence2 = "I am a comma seperated sentence";
+// let words2 = sentence2.split("a");
+// console.log(words2)
+
+// Q1. find out the occurance of "r" in the given string.
+// let str = "tu meri mai tera mai tera tu meri";
+// let count = str.split("r").length -1;
+
+// console.log(count)
+
+// setInterval(() =>{
+//     console.log("sign up")
+// } , 1000)
+
+// let date = new Date();
+// console.log(date)
+
+(function(){
+    console.log("IIFE");
+})()
