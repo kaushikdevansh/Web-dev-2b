3cc093e741c6734d4aea97de4b67ed9b05aaf6ff
Devansh <mailme.devanshkaushik@gmail.com>
2026-01-27 12:34:55 +0530

    script14

---
+++ script14.js
@@ -0,0 +1,21 @@
+// var a=10;
+// console.log(a);
+// function demo(){
+//     let b = 20;
+//     console.log(b);
+// }
+// demo()
+
+// let user = {
+//     FullName:"Devansh Kaushik",
+//     address:"Gurugram",
+//     mobileno:4343434343,
+//     favcolor: ["Blue" , "Red" , "Yellow"]
+// }
+// console.log(user.favcolor[1])
+
+const car = {
+    make: "Mahindra",
+    model: "Thar"
+};
+Object.seal(car);
